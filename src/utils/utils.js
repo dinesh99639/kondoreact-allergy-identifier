8ed2ee23d76e8fcb64b2f95298f823c5ff748b4c
Dinesh Somaraju <dinesh99639@gmail.com>
2025-03-21 00:30:06 +0530

    Add user and group parsers in utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -12,3 +12,49 @@ export const getCookie = (name) => {
 export const deleteCookie = (name) => {
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 };
+
+export const parseGroups = (groups) => {
+  if (!groups) return [];
+
+  return (
+    groups?.map((data) => {
+      const group = data.obj;
+
+      if (!group) return undefined;
+
+      return {
+        id: group?.id,
+        key: group?.name,
+        version: group?.version,
+        name: group?.custom?.fields?.groupName,
+
+        scanned: group?.custom?.fields?.scanned,
+        pending: group?.custom?.fields?.pending.map((user) =>
+          parseUserData(user.obj)
+        ),
+        accepted: group?.custom?.fields?.accepted.map((user) =>
+          parseUserData(user.obj)
+        ),
+        rejected: group?.custom?.fields?.rejected.map((user) =>
+          parseUserData(user.obj)
+        ),
+      };
+    }) || []
+  );
+};
+
+export const parseUserData = (user) => {
+  if (!user) return undefined;
+
+  return {
+    id: user?.id,
+    version: user?.version,
+    email: user?.email,
+    firstName: user?.firstName,
+    lastName: user?.lastName,
+
+    ailments: user?.custom?.fields?.ailments,
+    groups: parseGroups(user?.custom?.fields?.groups),
+    scanned: user?.custom?.fields?.scanned
+  };
+};
