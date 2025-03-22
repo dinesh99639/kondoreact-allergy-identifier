7aa46fc9b473c667a499f74e984a12d3b695b0e0
Vamsi <vamsijk4577@gmail.com>
2025-03-22 17:23:08 +0530

    Groups and Scanned Products

---
+++ src/pages/ExpiryProducts/ScannedProducts.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import { MdDeleteForever } from 'react-icons/md';
+import { FaHourglass } from 'react-icons/fa6';
+import { Tooltip } from '@progress/kendo-react-tooltip';
+
+const ScannedProducts = ({ scannedProducts, scannedByGroup, deleteItem }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: 'fit-content',
+        maxHeight: '60vh',
+        margin: 'auto',
+        overflow: 'hidden auto',
+      }}
+    >
+      {scannedProducts.map((sp, idx) => {
+        const backgroundColor =
+          sp[4] <= 7 ? 'red' : sp[4] > 7 && sp[4] <= 30 ? 'orange' : 'green';
+        return (
+          <div
+            style={{
+              padding: '1rem',
+              margin: '0.5rem',
+              display: 'flex',
+              width: '20rem',
+              justifyContent: 'space-between',
+              backgroundColor: backgroundColor,
+              boxShadow:
+                'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
+              color: 'white',
+            }}
+            key={idx}
+          >
+            <div>{sp[1]}</div>
+            <div
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '1rem',
+              }}
+            >
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Tooltip anchorElement={`target-${idx}`}>
+                  <span
+                    title={sp[2]}
+                    style={{
+                      display: 'inline-block',
+                    }}
+                  >
+                    <FaHourglass style={{ cursor: 'pointer' }} />
+                  </span>
+                </Tooltip>
+                &nbsp;{' '}
+                {sp[4] > 0 ? (
+                  <>
+                    Expires in {sp[4]} {sp[4] > 1 ? 'Days' : 'Day'}
+                  </>
+                ) : (
+                  <>
+                    Expired {Math.abs(sp[4])}{' '}
+                    {Math.abs(sp[4]) > 1 ? 'Days' : 'Day'} ago
+                  </>
+                )}
+              </div>
+              {!scannedByGroup && (
+                <MdDeleteForever
+                  style={{ cursor: 'pointer', fontSize: '1.25rem' }}
+                  title="Delete"
+                  onClick={() => {
+                    deleteItem(idx, sp[3]);
+                  }}
+                />
+              )}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default ScannedProducts;
---
+++ src/pages/Groups/Group.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { MdEdit } from 'react-icons/md';
+import { MdDeleteForever } from 'react-icons/md';
+import { FaChevronDown } from 'react-icons/fa';
+import { FaChevronUp } from 'react-icons/fa';
+
+const Group = ({ group }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggleAccordion = () => {
+    setIsOpen((prev) => !prev);
+  };
+  return (
+    <div>
+      <div
+        style={{
+          padding: '1rem',
+          margin: '0.5rem',
+          width: '20rem',
+          justifyContent: 'space-between',
+          display: 'flex',
+          boxShadow:
+            'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
+        }}
+      >
+        <div>{group.name}</div>
+        <div style={{ display: 'flex', gap: '0.5rem' }}>
+          <MdDeleteForever style={{ color: 'red', cursor: 'pointer' }} />
+          <MdEdit style={{ color: 'red', cursor: 'pointer' }} />
+          {!isOpen && (
+            <FaChevronDown
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={handleToggleAccordion}
+            />
+          )}
+          {isOpen && (
+            <FaChevronUp
+              style={{ color: 'red', cursor: 'pointer' }}
+              onClick={handleToggleAccordion}
+            />
+          )}
+        </div>
+      </div>
+
+      {isOpen && (
+        <div
+          style={{
+            margin: '0.5rem',
+            padding: '0.5rem',
+            paddingLeft: '2rem',
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '0.25rem',
+              color: 'rgb(123, 123, 123)',
+            }}
+          >
+            {group.accepted.map((user) => {
+              return (
+                <div>
+                  {user?.firstName} {user?.lastName}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Group;
