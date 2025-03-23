fc464b45d21f808ca57ed521e35df39dfa409734
hrithikpasumarthi <pasumarthihrithik@gmail.com>
2025-03-23 17:21:18 +0530

    add disclaimer

---
+++ src/data/constants.js
@@ -0,0 +1,4 @@
+export default  {
+    disclaimer:'Disclaimer',
+    disclaimerMessage : 'This app uses AI for informational purposes only and is not a substitute for professional medical advice. Always consult a healthcare provider. The developers are not liable for decisions or outcomes based on AI analysis.',
+}
